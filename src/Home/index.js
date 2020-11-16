b8f78fb914f4d12a1a0295dd3bd3965a40cc7cb6
BraboOliveira <rafaelgsn@live.com>
2020-11-16 02:08:52 -0300

    Novo

---
+++ src/Home/index.js
@@ -0,0 +1,148 @@
+import React , {useCallback, useEffect, useState} from 'react';
+import { View , Text, Button} from 'react-native';
+import database, { firebase } from '@react-native-firebase/database';
+ //import { Container } from './styles';
+
+const Home = () => {
+    const [valor, setValor] = useState([''])
+    const [dados, setDados] = useState([''])
+    const [tempo, setTempo] = useState(2000)
+    // useEffect(() => {
+    //     async function loadStorageData(){
+    //         console.log("uma vez aqui")
+    //         var credentials  = {
+    //             apiKey: "AIzaSyDdwitxShysQ4iFdiLD4hqobNSBNTbH1sE",
+    //             authDomain: "esp32-cc1a8.firebaseapp.com",
+    //             databaseURL: "https://esp32-cc1a8.firebaseio.com",
+    //             projectId: "esp32-cc1a8",
+    //             storageBucket: "esp32-cc1a8.appspot.com",
+    //             messagingSenderId: "579204333410",
+    //             appId: "1:579204333410:web:cdfa3e6d9d84d628f4763b"
+    //         };
+    //         const config = {
+    //             name: 'SECONDARY_APP',
+    //           };
+    //             // Initialize Firebase
+    //             const secondaryApp = await firebase.initalizeApp(credentials, config);
+    //         // firebase.initalizeApp(credentials, config);
+    //         }
+
+    //     loadStorageData()
+    // }, [])
+    // To Await 5 seconds to insert a new user
+    function enviaDado ()  {
+        firebase.database().ref('sensores').set(
+            {
+                luminosidade: 0,
+                umidade: 0,
+                temperatura: 0,
+                nh3:0,
+            }
+        ).then(() => {
+            console.log('INSERIDO !');
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+//Ouve o banco de dados
+useEffect(() => {
+    const interval = setInterval( ()=>{
+        firebase.database().ref('sensores/')
+        .once('value').then((snapshot) =>{
+            // console.log(snapshot.val())
+            var sensorData =snapshot.val()
+            let valorVal = valor.luminosidade;
+            let valor2 = sensorData.luminosidade
+            if(valorVal!=valor2){
+                setValor(sensorData)   
+            };
+                
+             });
+    },tempo)
+    return () => clearInterval(interval);
+},[]);
+
+    useEffect(() => {
+        console.log('teste')
+        if(valor.length > 0){
+        let newDados = {
+        luminosidade: valor.luminosidade,
+        umidade:valor.umidade,
+        temperatura:valor.temperatura,
+        nh3: valor.nh3,
+        }
+        let dadosA = Array.from(dados);
+        if(newDados.luminosidade!=''){
+            dadosA.push(newDados) 
+            console.log('atualiza')
+            setDados(dadosA)
+            //setDados([])
+        }
+    }  
+    },[valor.luminosidade]);
+
+    // setTimeout(()=>{
+    //     database()
+    //     .ref('/sensores')
+    //     .on('value', snapshot => {
+    //     // console.log('Dados do Usuário: ', snapshot.val());
+    //     setValor(snapshot.val());
+    //     });
+        
+    //     let newDados ={
+    //         luminosidade: valor.luminosidade,
+    //         umidade:valor.umidade,
+    //         temperatura:valor.temperatura,
+    //         nh3: valor.nh3,
+    //         }
+    //         let dadosA = dados
+    //         dadosA.push(newDados)
+    //         setDados(dadosA)
+    //         console.log(dados)
+    // },[20000])
+
+    // useEffect(() => {
+    //     console.log('mudou')
+    //   }, [valor]); // Apenas re-execute o efeito quando o count mudar
+
+     inserirDados = ()=>{
+        // let newDados ={
+        // luminosidade: valor.luminosidade,
+        // umidade:valor.umidade,
+        // temperatura:valor.temperatura,
+        // nh3: valor.nh3,
+        // }
+        // let dadosA = dados
+        // dadosA.push(newDados)
+
+        console.log(dados.length)
+        console.log(dados)
+    }
+    // const data = [
+    //     {luminosidade: valor.luminosidade},
+    //     {umidade:valor.umidade},
+    //     {temperatura:},
+    //     {nh3},
+    // ]
+
+  return (
+  <View>
+      <Text>Luminopsidade: {valor.luminosidade}</Text>
+      <Text>Temperatura: {valor.temperatura}</Text>
+      <Text>umidade: {valor.umidade}</Text>
+      <Text>Nh3: {valor.nh3}</Text>
+      <Button
+        title="Press me"
+        onPress={enviaDado}
+      />
+        <Button
+        title="dados"
+        onPress={inserirDados}
+      />
+  </View>
+  )
+}
+
+export default Home;
+
---
+++ src/Home/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+
+	justify-content: center;
+`
+// [{"luminosidade": 0, "nh3": 0, "temperatura": 0, "umidade": 0}, {"luminosidade": undefined, "nh3": undefined, "temperatura": undefined, "umidade": undefined}, {"luminosidade": "02", "nh3": 0, "temperatura": 0, "umidade": 0}, {"luminosidade": "02", "nh3": 0, "temperatura": 0, "umidade": 0}, {"luminosidade": "02", "nh3": 0, "temperatura": 0, "umidade": 0}, {"luminosidade": "02", "nh3": 0, "temperatura": 0, "umidade": 0}]
